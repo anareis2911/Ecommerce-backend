87589f425d06ee862ffc48d3bcf5839acf61ed9d
ANA CAROLINA REIS <ana.reis4@alunos.sc.senac.br>
2024-11-26 19:33:53 -0300

    First commit

---
+++ aula01/src/main/java/org/senac/aula01/controller/ProdutoController.java
@@ -0,0 +1,31 @@
+package org.senac.aula01.controller;
+
+import java.util.List;
+
+import org.senac.aula01.model.Produto;
+import org.senac.aula01.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoRepository repository;
+
+    @GetMapping
+    public List<Produto> get(){
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Produto save(@RequestBody Produto produto){
+        return repository.save(produto);
+    }
+    
+}
---
+++ aula01/src/main/java/org/senac/aula01/model/Produto.java
@@ -0,0 +1,49 @@
+package org.senac.aula01.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(name="description", length = 255)
+    private String descricao;
+
+    private float preco;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public float getPreco() {
+        return preco;
+    }
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+    
+}
